336618a7fd2a1c8f1133b6c69f93de97fa95d80b
Mostafa Ahmed <141432957+T1faa@users.noreply.github.com>
2025-11-11 09:31:47 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -14,6 +14,28 @@ class Course {
     int currentStudents;
 
     public:
+
+    Course();
+    ~Course();
+
+    Course(string code, string name, int max, Student* stud, int current);
+
+    string getCourseCode();
+    void setCourseCode(string m);
+
+    string getCourseName();
+    void setCourseName(string m);
+
+    int getMaxStudents();
+    void setMaxStudents(int x);
+
+    Student* getStudents();
+    void setStudents(Student* x);
+
+    int getCurrentStudents();
+    void setCurrentStudents(int x);
+
+
     void addStudent(const Student& s);
     void displayCourseInfo();
 };
