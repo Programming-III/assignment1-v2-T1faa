ae112b525f53c31a4017e88dc07b5bb6e82029f6
Mostafa Ahmed <141432957+T1faa@users.noreply.github.com>
2025-11-11 08:56:07 +0200

    Define Person class with private attributes

--- Person.h
+++ Person.h
@@ -4,16 +4,16 @@
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+    private:
+    string name;
+    int id;
 
 
+    public:
 
-
-
-
-
-
-
+    void display(); 
+};
 
 
 
