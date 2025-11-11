d8ad4d9494691fe38c254da9ea4bafb66e48cb04
Mostafa Ahmed <141432957+T1faa@users.noreply.github.com>
2025-11-11 09:27:18 +0200

    Fix inheritance syntax for Instructor class

--- Instructor.h
+++ Instructor.h
@@ -4,13 +4,25 @@
 #include "Person.h"
 using namespace std;
 
-class Instructor :: Person {
-    
+class Instructor : public Person {
+
     private:
     string department;
     int experienceYears;
 
     public:
+
+    Instructor();
+    ~Instructor();
+
+    Instructor(string dep, int years, string n, int _id) : Person(n, _id){};
+
+    string getDepartment();
+    void setDepartment(string m);
+
+    int getExperienceYears();
+    void setExperienceYears(int x);
+    
     void display();
 };
 
