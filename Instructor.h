befd1629f255b4dc829d44880a8dd1f7d027a4c0
Mostafa Ahmed <141432957+T1faa@users.noreply.github.com>
2025-11-11 09:09:11 +0200

    Fix class declaration and header guard in Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -1,10 +1,12 @@
-  #ifndef INSTRUCTOR_H
+#ifndef INSTRUCTOR_H
 #define INSTRUCTOR_H
 
 #include "Person.h"
 using namespace std;
 
-class instructor :: person {
+class Instructor :: Person {
+    
+    private:
     string department;
     int experienceYears;
 
